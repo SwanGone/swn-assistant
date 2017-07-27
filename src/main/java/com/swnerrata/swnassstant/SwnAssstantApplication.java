43a47f51c21b17c560e8d9325745b2f72f416ca1
SwanGone <greathairrecords@gmail.com>
2017-07-27 11:06:10 -0500

    First commit

---
+++ src/main/java/com/swnerrata/swnassstant/SwnAssstantApplication.java
@@ -0,0 +1,12 @@
+package com.swnerrata.swnassstant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwnAssstantApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SwnAssstantApplication.class, args);
+	}
+}
